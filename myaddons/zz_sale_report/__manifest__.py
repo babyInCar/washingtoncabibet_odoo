d41e065d7ee29c568429c1599e21c729dd61d453
theyrfm <2208300811@qq.com>
2022-12-17 21:52:28 +0800

    上传报表模块

---
+++ myaddons/zz_sale_report/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "zz_sale_report",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "zz",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale', 'web', 'account', 'stock'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/sale_order_views.xml',
+        'views/stock_picking_views.xml',
+        'report/report_sale_order.xml',
+        'report/report_invoice.xml',
+        'report/report_stockpicking_operations.xml',
+        'report/report_deliveryslip.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
