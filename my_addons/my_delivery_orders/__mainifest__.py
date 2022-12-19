28356c011e2463673355946dc7a01c3d6476ef9a
babyInCar <gaoshuang916@126.com>
2022-12-19 21:56:59 +0800

    update my_stock_delivery

--- my_addons/my_delivery_orders/__mainifest__.py
+++ my_addons/my_delivery_orders/__mainifest__.py
@@ -1,7 +1,7 @@
 
 # -*- coding: utf-8 -*-
 {
-    'name': '发货管理',
+    'name': 'delivery manage',
     'version': '0.1',
     'summary': '发货模块添加新功能',
     'description': '',
@@ -9,9 +9,9 @@
     'author': 'gaoshuang916@126.com',
     'depends': ['base', 'sale', 'web', 'account', 'stock'],
     'data': [
-        # 'security/access_security.xml',
-        # 'security/ir.model.access.csv',
+        #  'security/ir.model.access.csv',
         'views/stock_pick_views.xml',
     ],
-    # 'installable': True,
+    'installable': True,
+    'application': True,
 }
