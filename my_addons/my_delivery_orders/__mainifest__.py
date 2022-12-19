41b16913ce7271a2d87d23cd1f6aba65b1c6b9e7
babyInCar <gaoshuang916@126.com>
2022-12-19 17:19:07 +0800

    库存模块的修改

---
+++ my_addons/my_delivery_orders/__mainifest__.py
@@ -0,0 +1,17 @@
+
+# -*- coding: utf-8 -*-
+{
+    'name': '发货管理',
+    'version': '0.1',
+    'summary': '发货模块添加新功能',
+    'description': '',
+    'category': 'Uncategorized',
+    'author': 'gaoshuang916@126.com',
+    'depends': ['base', 'sale', 'web', 'account', 'stock'],
+    'data': [
+        # 'security/access_security.xml',
+        # 'security/ir.model.access.csv',
+        'views/stock_pick_views.xml',
+    ],
+    # 'installable': True,
+}
